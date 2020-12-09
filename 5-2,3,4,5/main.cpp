21556949809cf75add87a33ba00e2cb069ed6581
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-09 17:48:32 +0300

    5

--- 5-2,3,4,5/main.cpp
+++ 5-2,3,4,5/main.cpp
@@ -1,14 +1,47 @@
-//
-//  main.cpp
-//  5-2,3,4,5
-//
-//  Created by Никита Кузнецов on 09.12.2020.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+//2 Задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a, b, c, ac, bc;
+//    cin >> a >> b >> c;
+//    ac = abs(a-c);
+//    bc = abs(b-c);
+//    cout << "ac = " << ac << "\nbc = " << bc << "\nsum = " << ac+bc;
+//}
+//3 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double a, b, c, ac, bc;
+//    cin >> a >> b >> c;
+//    ac = abs(a-c);
+//    bc = abs(b-c);
+//    cout << ac*bc;
+//}
+//4 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double x1, x2, y1, y2;
+//    cin >> x1 >> y1 >> x2 >> y2;
+//    double a = abs(x1-y1);
+//    double b = abs(x2-y2);
+//    cout << "\nP = " << a+b+a+b << "\nS = " << a*b;
+//}
+//5 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main(){
+//    double x1, x2, y1, y2, x3, y3;
+//    cin >> x1 >> x2 >> y1 >> y2 >> x3 >> y3;
+//    double a = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+//    double b = sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2));
+//    double c = sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
+//    double P = a+b+c;
+//    double S = 0.5*(abs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1)));
+//    cout << "\nP = " << P << "\nS = " << S;
+//}
